8f5f940c494d05c1426fea2dbe71e3fe9cc2488e
tchen4 <tchen4@uw.edu>
2023-03-06 19:29:25 -0800

    deployed my site

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import copy from "rollup-plugin-copy";
+
+module.exports = {
+  input: "index.js",
+  output: {
+    dir: "dist",
+  },
+  treeshake: false,
+  plugins: [
+    copy({
+      targets: [{ src: "index.html", dest: "dist" }],
+    }),
+    nodeResolve(),
+  ],
+};
